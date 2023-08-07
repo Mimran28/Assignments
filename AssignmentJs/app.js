2cfb56f83e679566d500ff8fc306ddddf99befbf
Mimran28 <58678641+Mimran28@users.noreply.github.com>
2023-08-07 08:07:26 -0700

    update

---
+++ AssignmentJs/app.js
@@ -0,0 +1,82 @@
+// Chapter 1
+
+// alert("Welcome to our website");
+// alert(" Error! Please Enter Valid Password");
+// alert("Welcome to js land \n Happy coding");
+// alert("Welcome to js land");
+
+//Chapter 2
+
+// // var username;
+// // var fullname = "imran";
+// // var message = "Hello World";
+// // alert(message);
+// //  var fname = "John doe";
+// //  var age = "15 years old";
+// //  var qual = "Qualification";
+// //  alert(fname);
+// //  alert(age);
+// //  alert(qual);
+//  var pizza= "pizza\n pizz \n piz \n pi \n p"
+//  alert(pizza);
+// var email ="@email.com";
+// alert("Your email"+email);
+// var book = "A smarter way to learn js";
+// alert("i am trying to "+book);
+// document.write = "hello";
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+//Chapter 3
+// var age = 25;
+// alert(age);
+
+// var visitor = 0;
+// alert("you have visited"+visitor+"times");
+// var birthYear = 123;
+// document.write(birthYear);
+
+// var visistorName = "xyz";
+// var productTitle = "title";
+// var quantity = 24;
+
+// document.write(visistorName+"ordered"+quantity+"on store");
+
+//Chapter 4
+
+// var variableOne ;
+// var variableTwo;
+// var  variableThree;
+
+// var 1variable;
+// var variable%;
+// var new@variable;
+// var 3variable;
+// var variable a;
+
+// var helloWorld;
+// var hell02;
+// var _hello;
+// var $hello;
+// var new_variable;
+
+// document.write("Rules for defining js");
+
+// document.write("variable name can only contain $, _,alphabets and  \n For Example $my_1stVariable");
+// document.write("documents must begin with alphabet ,$ and _ \n For example $name, _name or name");
+// document.write ("variable names are case sensitive");
+// document.write("variable names should not be js keywords")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
