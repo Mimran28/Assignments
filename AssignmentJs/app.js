0b349dcf84134fe2781ae4f77057fc9551630324
Mimran28 <58678641+Mimran28@users.noreply.github.com>
2023-08-08 11:47:48 -0700

    update

--- AssignmentJs/app.js
+++ AssignmentJs/app.js
@@ -68,9 +68,111 @@
 // document.write("variable names should not be js keywords")
 
 
+//Chapter 5
+
+// var num1= 3;
+// var num2 = 5;
+// var result = num1+num2;
+
+// document.write("Sum of 3 and 5 is "+result);
+
+// var num1= 3;
+// var num2 = 5;
+// var result = num2-num1;
+
+// document.write("subtraction of 5 and 3 is "+result);
+
+
+// var num1= 3;
+// var num2 = 5;
+// var result = num2*num1;
+
+// document.write("multiplication of 5 and 3 is "+result);
+
+
+// var num1= 3;
+// var num2 = 5;
+// var result = num2/num1;
+
+// document.write("division of 5 and 3 is "+result);
+
+// var _sum;
+// document.write(_sum+"<br/>");
+// _sum=12;
+// document.write("Initial value "+_sum+"<br/>");
+// _sum++;
+// document.write("Value after increment "+ _sum +"<br/>");
+// var updt = _sum+7;
+// document.write("Value "+updt +"<br/>");
+// _sum--;
+// document.write("Value after decrement "+ _sum +"<br/>");
+// _sum/3;
+// document.write("The reminder is : "+ 0);
+
+// var movie_ticket = 600;
+// document.write("Price of Ticket ="+movie_ticket+"<br/>");
+// var movie5= 5;
+// var cost = 600*movie5;
+// document.write("5 Ticket price ="+cost);
+
+// document.write("Table of 4 <br/>");
+// document.write("4x1=4 <br/>4x2=8 <br/>4x3=12 <br/>4x4=16 <br/>4x5=20 <br/>4x6=24 <br/>4x7=28 <br/>4x8=32<br/>4x9=36 <br/>4x10=40 <br/>")
+
+
+// var temp = 30
+// document.write("temp in Celcius "+ temp+"<br/>");
+// var ftemp = (temp-32)*5/9;
+
+// document.write("Temp in Fahrenheit "+ ftemp);
+
+// var item1 = 650;
+// var qty1 = 3;
+// var item2 = 100;
+// var qty2 =7;
+// var charges= 100;
+// var result = item1*qty1+item2*qty2;
+
+
+// document.write("<h1>Shopping Cart</h1><br/>");
+
+// document.write("Price of item 1 is "+ item1+"<br/>");
+// document.write("Quantity of item 1 is "+qty1+"<br/>");
+// document.write("Price of item 2 is "+item2+"<br/>");
+// document.write("Quantity of item 2 is "+ qty2+"<br/>");
+// document.write("Shipping Charges is "+ charges+"<br/><br/><br/>");
+// document.write("Total Cost of your order is "+ result+"<br/>");
+
+// var total=980;
+// var obtainedmarks = 804;
+// var percent = obtainedmarks/total*100;
+// document.write("<h1>Marksheet</h1><br/>");
+// document.write("Total Marks: "+total+"<br/>");
+// document.write("Marks obtained: "+ obtainedmarks+"<br/>");
+// document.write("Percentage "+ percent);
+
+// var dollar = 10;
+// var riyal = 25;
+// var totalpkr = 10*288+25*77;
+
+// document.write("Total Currency in PKR: "+ totalpkr);
+
+// var num = 5;
+// num=num+5;
+// num=num*10;
+// num=num/2;
+
+// document.write(num);
 
 
 
+
+// var birthYear= 1998;
+// var currentYear = 2023
+// var age =currentYear-birthYear
+// document.write("<h1>Age Calculator</h1><br/>");
+// document.write("Current Year "+currentYear+"<br/>");
+// document.write("Birth Year "+birthYear+"<br/>");
+// document.write(" Your Age "+age+"<br/>");
 
 
 
